dafcc6c54c3712a00b7edb8607130a6a9b34317d
Ada Void <oxiggy@oxiggy.com>
2021-06-09 17:15:54 +0200

    build(mocks): мокинг методов входа

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,34 @@
+const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
+
+  // app.get('/api/v2/user', (req, res) => {
+  //   if (req.headers.authorization === 'Bearer 123.456.789') {
+  //     res.send({
+  //       id: 1
+  //     })
+  //   } else {
+  //     res.status(401)
+  //     res.send({
+  //       message: 'not authenticated'
+  //     })
+  //   }
+  // })
+
+  // app.post('/api/v2/user/sign-in', express.json(), (req, res) => {
+  //   const { email, password } = req.body
+  //   if (email === '1@1' && password === '1') {
+  //     res.send({
+  //       token: '123.456.789'
+  //     })
+  //   } else {
+  //     res.status(401)
+  //     res.send({
+  //       message: 'invalid credentials'
+  //     })
+  //   }
+  // })
 
   app.use(
     '/api',
